1c56eab09537a8e3f9c0626906f234e7e02cc6d8
mansi-0211 <89212022+mansi-0211@users.noreply.github.com>
2021-08-27 21:25:02 +0530

    Add files via upload

---
+++ Simple-Assembler/code.py
@@ -0,0 +1,272 @@
+global var_list
+var_list=[]
+
+global label_list
+label_list = []
+
+global Error
+Error =0
+
+def assembly_to_binary(l,c):
+    global var_list
+    global register
+    global Error
+    global label_list
+    bin_=""
+    
+
+    line = list(map(str ,l.split()))
+
+
+    
+    for i in c:
+        if i=="hlt" and c.index(i)!= len(c)-1:
+            Error=1
+            return("hlt not being used as the last instruction")
+    
+    s = c[-1]
+    search = list(map(str,s.split()))
+
+    if "hlt" not in search:
+        Error=1
+        return("missing hlt statement")
+    
+    if "FLAGS" in line and "mov" not in line :
+        Error =1
+        return("illegal use of flags")
+    key_values=[]   
+    dictionary = {"R0":"000", "R1" : "001" ,"R2" : "010", "R3" : "011", "R4" : "100", "R5": "101","R6" : "110","FLAGS": "111"}
+    for k in dictionary.keys():
+        key_values.append(k)
+    
+    type_A = ["add","sub","mul","xor","or","and"]
+    type_B = ["mov","rs","ls"]
+    type_C = ["mov","div","not","cmp"]
+    type_D = ["ld","st"]
+    type_E = ["jmp","jlt","jgt","je"]
+    type_F = ["hlt"]
+    
+    type_=type_A+type_B+type_C+type_D+type_E+type_F
+    
+    if (type_A.count(line[0]) > 0):
+  
+        if len(line)<4 :
+            Error =1
+            return("General Syntax Error")
+        
+        if (line[0] == "add"):
+            bin_ = "00000"
+                
+        elif(line[0] == "sub"):
+            bin_ = "00001"
+
+                
+        elif(line[0] == "mul"):
+            bin_ = "00110"
+
+                
+        elif(line[0] == "xor"):
+            bin_ = "01010"
+        elif(line[0] == "or"):
+            bin_ = "01011"
+        elif(line[0] == "and"):
+            bin_ = "01100"
+        
+            
+            
+        bin_ = bin_ + "00"
+        if (key_values.count(line[1])>0 and key_values.count(line[2])>0 and key_values.count(line[3])>0): 
+            bin_  = bin_ + dictionary[line[1]]
+            bin_  = bin_ + dictionary[line[2]]
+            bin_ = bin_ + dictionary[line[3]]
+            
+       
+    elif (type_B.count(line[0]) > 0 and line[2] not in key_values):
+   
+        if(line[0] == "mov" and key_values.count(line[2]) == 0):
+            bin_ = "00010"
+            
+        elif(line[0] == "rs"):
+            
+            bin_ = "01000"
+        elif(line[0] == "ls"):
+            bin_ = "01001"
+       
+        else:
+            Error =1
+            return("General Syntax Error")
+        
+            
+        
+        bin_  = bin_ + dictionary[line[1]]
+       
+        a = int(line[2][1:])
+  
+        if (0 <= a <= 255): 
+            bnr = bin(a).replace('0b','')
+            x = bnr[::-1]
+            while len(x) < 8:
+                
+                x += '0'
+            bnr = x[::-1]
+            bnr = str(bnr)
+            bin_ = bin_ + bnr
+         
+        
+        else:
+            Error =1
+            return("Illegal Immediate values")
+            
+        
+        
+    elif (type_C.count(line[0]) > 0):
+
+        if(line[0] == "mov" and key_values.count(line[2]) == 1 ):
+            bin_ = "00011"
+
+        elif(line[0] == "div"):
+            bin_ = "00111"
+        elif(line[0] == "not"):
+            bin_ = "01101"
+        elif(line[0] == "cmp"):
+            bin_ = "01110"
+
+        bin_  = bin_ + "00000"
+        bin_  = bin_ + dictionary[line[1]]
+        bin_  = bin_ + dictionary[line[2]]
+        
+    elif (type_D.count(line[0]) > 0):
+        if(line[0] == "ld"):
+            bin_ = "00100"
+            
+            if(line[2] not in var_list ):
+                Error =1
+                return("Use of undefined variables")
+            
+        elif(line[0] == "st"):
+            bin_ = "00101"
+            if (line[2] in var_list):
+                bin_  = bin_ + dictionary[line[1]]
+                mem_ad= bin(len(c) + var_list.index(line[2]))
+                mem_ad = mem_ad[2:]
+                a = mem_ad[::-1] 
+                while len(a) < 8:
+                    a += '0'
+                mem_ad = a[::-1]
+                bin_  = bin_ + str(mem_ad)
+                
+#            elif (int(line[2],2) == int and int(line[2],2)<=len(c)):            
+#                bin_  = bin_ + dictionary[line[1]]
+#                mem_ad= line[2]
+#                bin_  = bin_ + str(mem_ad)
+            elif(line[2] not in var_list) :
+                Error =1
+                return("Use of undefined variables")
+            
+            
+        
+
+        
+    elif (type_E.count(line[0]) > 0):
+      
+        if(line[0] == "jmp"):
+            bin_ = "011111"
+        elif(line[0] == "jlt"):
+            bin_ = "10000"
+        elif(line[0] == "jgt"):
+            bin_ = "10001"
+        elif(line[0] == "je"):
+            bin_ = "10010"
+            
+        bin_  = bin_ + "000"
+
+        for i in c :
+            parts = list(map(str,i.split()))
+            if line[1] == parts[0]:
+                label_ad = bin(c.index(i)).replace('0b','')
+                w = label_ad[::-1]
+                while len(w)<8:
+                    w = w+"0"
+                label_ad = w[::-1]
+                bin_ = bin_ + label_ad
+                
+        
+                
+        
+    elif (type_F.count(line[0]) > 0):
+        bin_ = "10011"
+        bin_  = bin_ + ("0" * 11)
+    
+    
+    
+    
+    
+    elif (line[0][-1] == ":"):
+        if type_.count(line[0][:-1]) > 0 :
+            Error = 1
+            return("Command name has been used as the name of label")
+        elif (label_list.count(line[0][:-1]) > 0):
+            Error = 1
+            return("A label has already been assigned by this name")
+    
+        else:
+            label_list.append(line[0][:-1])
+            Z = ""
+            for i in line[1:]:
+                Z = Z + i + " "
+            return(assembly_to_binary(Z,c))
+       
+    
+    
+    
+    return (bin_)
+    
+    
+    
+
+
+code=[]
+while True :
+    try:
+        i=input()
+       
+        if len(i)!=0:
+             code.append(i)
+    except EOFError:
+        break
+    
+
+
+for command in code :
+    if command[:3] == "var":
+        code.remove(command)
+        var_list.append(command[4:])
+    else:
+        break
+
+
+
+
+
+
+
+for command in code:
+    if command != "hlt":
+        
+        command=command[:-1]
+
+        
+   
+
+
+
+
+
+for command in code:
+    print(assembly_to_binary(command, code))
+    
+    
+    if Error == 1:
+        temp=assembly_to_binary(command, code)
+        print(temp)
+        break
